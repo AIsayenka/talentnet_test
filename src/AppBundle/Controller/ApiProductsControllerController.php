824b29288a5c5f6d42e31cfc6f73e580f0cea52e
Alex Isayenka <aliaksei.isayenka@gmail.com>
2018-01-23 23:32:13 -0500

    Adding actions to product management api

---
+++ src/AppBundle/Controller/ApiProductsControllerController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ApiProductsControllerController extends Controller
+{
+    public function get_allAction()
+    {
+
+    }
+
+    public function get_oneAction($id)
+    {
+
+    }
+
+    public function create_recordAction()
+    {
+
+    }
+
+    public function update_recordAction($id)
+    {
+
+    }
+
+    public function delete_recordAction($id)
+    {
+        
+    }
+
+}
